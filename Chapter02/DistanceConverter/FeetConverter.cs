889a88b7a4aa638bd065fef319716de031ab9c1e
33183YasudaTsuyoshi <33183@ojs.ac.jp>
2022-04-26 14:03:36 +0900

    定数ratioを定義

--- Chapter02/DistanceConverter/FeetConverter.cs
+++ Chapter02/DistanceConverter/FeetConverter.cs
@@ -7,14 +7,18 @@ using System.Threading.Tasks;
 namespace DistanceConverter {
     public static class FeetConverter {
 
+        private const double ratio = 0.3048; //定数ratioを定義
+
+        //public  static readonly ratio = 0.3048; «外部からもアクセスさせたい場合
+
         //静的メソッド
         public static double FromMeter(double meter) {
-            return meter / 0.3048;
+            return meter / ratio;
         }
 
         //静的メソッド
         public static double ToMeter(double feet) {
-            return feet * 0.3048;
+            return feet * ratio; //ratioマジックナンバー
         }
     }
 }
