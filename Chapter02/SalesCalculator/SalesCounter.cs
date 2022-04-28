ff2383a0600b1022b17f9ef23e70da932533600e
33183YasudaTsuyoshi <33183@ojs.ac.jp>
2022-04-28 14:04:14 +0900

    初版の完成(p68)

---
+++ Chapter02/SalesCalculator/SalesCounter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalculator {
+    public class SalesCounter {
+        
+        private List<Sale> _sales;//csvファイルから読み込んだデータ
+
+        public SalesCounter(List<Sale> sales) {
+            _sales = sales;
+        }
+
+        public Dictionary<string, int> GetPerStoreSales() {
+
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            foreach (Sale sale in _sales) {
+                if (dict.ContainsKey(sale.ShopName)) {
+                    dict[sale.ShopName] += sale.Amount;
+                }
+                else {
+                    dict[sale.ShopName] = sale.Amount;
+                }
+            }
+            return dict;
+        }
+    }
+}
