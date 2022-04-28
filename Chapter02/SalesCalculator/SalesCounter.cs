1c6c8e34d24952bac8271b0c50794f523669039b
33183YasudaTsuyoshi <33183@ojs.ac.jp>
2022-04-28 15:05:54 +0900

    メソッドの移動

--- Chapter02/SalesCalculator/SalesCounter.cs
+++ Chapter02/SalesCalculator/SalesCounter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 namespace SalesCalculator {
     public class SalesCounter {
         
-        private List<Sale> _sales;//csvファイルから読み込んだデータ
+        private List<Sale> _sales; //csvファイルから読み込んだデータ
 
         public SalesCounter(List<Sale> sales) {
             _sales = sales;
@@ -27,5 +28,23 @@ namespace SalesCalculator {
             }
             return dict;
         }
+
+        //売り上げデータを読み込み、Saleオブジェクトのリストを返す
+        public static  List<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
+
+        }
+
     }
 }
