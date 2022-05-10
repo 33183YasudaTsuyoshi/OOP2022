7629a16f981e72d8279d8d5b419c2ab8e46cbdfe
33183YasudaTsuyoshi <33183@ojs.ac.jp>
2022-05-10 15:24:51 +0900

    問題 2.1.3完成

--- Chapter02/Exercise/Program.cs
+++ Chapter02/Exercise/Program.cs
@@ -8,6 +8,27 @@ namespace Exercise {
     class Program {
         static void Main(string[] args) {
 
+            /*List<Song> songs = new List<Song>();
+
+            var songs1 = new Song("a", "b", 5);
+            songs.Add(songs1);*/
+
+            var songs = new Song[] {
+                new Song("a", "b", 594),
+                new Song("a", "b", 432),
+                new Song("a", "b", 343),
+                new Song("a", "b", 255),
+            };
+
+            PrintSongs(songs);
+
+        }
+        private static void PrintSongs(Song[] songs) {
+            
+        }
+
+        private static void PrintSongs(List<Song> songs) {
+            
         }
     }
 }
