97205fc4ff082a75dd243af6770924f21d8cb9db
33183YasudaTsuyoshi <33183@ojs.ac.jp>
2022-05-12 14:30:40 +0900

    問題2.2完成

---
+++ Chapter02/Exercise02/Calculation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Calculation {
+
+        private int inch;
+        private const double meter = 0.0254;
+
+        public static double FromMeter(double inch) {
+
+            return inch * meter;
+
+        }
+        
+    }
+}
---
+++ Chapter02/Exercise02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+
+            PrintMeterToInList(1, 10);
+
+        }
+
+        private static void PrintMeterToInList(int start, int stop) {
+
+            for (int inch = start; inch <= stop; inch++) {
+                double meter = Calculation.FromMeter(inch);
+                Console.WriteLine("{0}in  ={1:0.0000} m", inch, meter);
+            }
+
+        }
+    }
+}
