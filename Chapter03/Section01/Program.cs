42c86e5b249f794c64ebead2dde89fb05c9a6f5f
33183YasudaTsuyoshi <33183@ojs.ac.jp>
2022-05-13 15:22:20 +0900

    ラクダ式の組み合わせ

---
+++ Chapter03/Section01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+
+        public delegate bool Judgement(int value); //デリゲートの宣言
+
+        static void Main(string[] args) {
+            var list = new List<string> {
+               "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+            var exissts = list.Exists(s => s[0] == 'A');//最初の文字がAの時
+            Console.WriteLine(exissts);
+
+            var name = list.Find(s => s.Length == 6); //一つ6文字を出力
+            Console.WriteLine(name);
+
+           /* var names = list.FindAll(s => s.Length <= 5); //5文字以下すべて
+            foreach (var s in names) {
+                Console.WriteLine(s);
+            }*/
+
+            var names = list.FindAll(s => s.Length <= 5);
+            names.ForEach(s => Console.WriteLine(s));
+
+
+        }
+    }
+}
