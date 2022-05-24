00f530113a1df4e4c80c52c304279a0a267079f4
33183YasudaTsuyoshi <33183@ojs.ac.jp>
2022-05-24 13:51:29 +0900

    第4章問題完成　Exercise01参照

---
+++ Chapter04/Exercise02/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using Exercise01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+
+            int[] nums = new int[] {
+                4,
+                5,
+                6,
+                7,
+                8,
+            };
+
+            //
+
+        }
+    }
+}
