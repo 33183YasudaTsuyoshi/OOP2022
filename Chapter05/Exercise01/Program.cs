33cd98ec6f89c7a88778ceacc23d11c3c76b3a5f
33183YasudaTsuyoshi <33183@ojs.ac.jp>
2022-05-26 15:27:15 +0900

    第5章 5.1完成

---
+++ Chapter05/Exercise01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("入力1");
+            var str1 = Console.ReadLine();
+            Console.WriteLine("入力2");
+            var str2 = Console.ReadLine();
+
+            var result = string.Compare(str1, str2, true);
+
+            if (result == 0) {
+                Console.WriteLine("一致しています");
+            } else {
+                Console.WriteLine("一致していません");
+            }
+
+        }
+    }
+}
