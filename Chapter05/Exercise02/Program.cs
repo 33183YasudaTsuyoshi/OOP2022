aee6b4ec949a0b7376b72f5eb4a3846555783667
33183YasudaTsuyoshi <33183@ojs.ac.jp>
2022-05-26 15:42:03 +0900

    第5章 5.2 完成

---
+++ Chapter05/Exercise02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("入力1");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(string.Format("{0:#,0.0}", num));
+             
+        }
+    }
+}
