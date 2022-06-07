72bd659bf2c7b289de8e89475c75378c3c186c82
33183YasudaTsuyoshi <33183@ojs.ac.jp>
2022-06-07 13:15:01 +0900

    フォーマット計算

---
+++ FormAppSample/Sample0603/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sample0603 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        //ボタンクリックイベントハンドラ
+        private void button1_Click(object sender, EventArgs e) {
+
+            //tbAns.Text = (int.Parse(tbSuu1.Text) + int.Parse(tbSuu2.Text)).ToString();
+            numericUpDown4.Value = numericUpDown2.Value + numericUpDown3.Value;
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void numericUpDown4_ValueChanged(object sender, EventArgs e) {
+
+        }
+    }
+}
