cd3f7a275137998344acaf5e4026e07745b2f4f4
33183YasudaTsuyoshi <33183@ojs.ac.jp>
2022-06-14 15:36:01 +0900

    統一試験完成

--- Test202206/TestProject/Test02/Program.cs
+++ Test202206/TestProject/Test02/Program.cs
@@ -39,6 +39,7 @@ namespace Test02 {
 
             var sum = numbers.Sum();
             Console.WriteLine(sum);
+
         }
 
         //問題２　偶数の最大値を表示
@@ -47,27 +48,30 @@ namespace Test02 {
 
             var max = numbers.Where(n => n % 2 == 0).Max(n => n);
             Console.WriteLine(max);
+
         }
         //問題３　昇順に並べて表示（遅延実行とする）
         //　　　　出力結果【12 14 17 20 31 35 40 48 53 76 87 91 94】
         private static void Exercise03(List<int> numbers) {
 
             var sum = numbers.OrderBy(n => n);
+
             foreach (var s in sum) {
                 Console.WriteLine(s);
+
             }
-            
         }
 
         //問題４　10以上50以下の数字のみを表示（即時実行でも可とする）
         //　　　　出力結果【12 14 20 40 35 31 17 48】
         private static void Exercise04(List<int> numbers) {
 
-            //var sum = numbers.Where(n => n >= 10 );
+            var sum = numbers.Where(n => n >= 10 ).Where(n => n <= 50);
             
-            /*foreach (var s in sum) {
+            foreach (var s in sum) {
                 Console.WriteLine(s);
-            }*/
+
+            }
         }
     }
 }
