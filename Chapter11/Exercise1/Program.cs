9afe478f32623adefe89c0219d8ef4b32b26efcd
33183YasudaTsuyoshi <33183@ojs.ac.jp>
2022-07-04 11:57:51 +0900

    第11章の演習問題　11.1.1  ,  11.1.2完成

---
+++ Chapter11/Exercise1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.IO;
+
+namespace Exercise1 {
+    class Program {
+        static void Main(string[] args) {
+
+            var file = "sample.xml";
+
+            Exercise1_1(file);
+            Console.WriteLine();
+
+            Exercise1_2(file);
+            Console.WriteLine();
+
+            Exercise1_3(file);
+            Console.WriteLine();
+
+            var newfile = "sports.xml";
+            Exercise1_4(file, newfile);
+        }
+
+        private static void Exercise1_1(string file) {
+
+            var xdoc = XDocument.Load(file);
+            var novelists = xdoc.Root.Elements()
+                                .Select(x => new {
+                                    Name = (string)x.Element("name"),
+                                    teammember = (string)x.Element("teammembers"),
+                                });
+
+            foreach (var novelist in novelists) {
+
+                Console.WriteLine("{0} {1}",
+                                   novelist.Name, novelist.teammember);
+
+            }
+        }
+
+        private static void Exercise1_2(string file) {
+
+            var xdoc = XDocument.Load(file);
+            var novelists = xdoc.Root.Elements()
+                                .Select(x => new {
+                                    Name = (string)x.Element("name").Attribute("kanji"),
+                                    firstplayeds = (string)x.Element("firstplayed"),
+
+                                });
+
+            foreach (var novelist in novelists.OrderBy(x => x.firstplayeds)) {
+
+                Console.WriteLine("{0}({1})", novelist.Name, novelist.firstplayeds);
+
+            }
+
+        }
+
+        private static void Exercise1_3(string file) {
+
+            var xdoc = XDocument.Load(file);
+            var novelists = xdoc.Root.Elements()
+                                .Select(x => new {
+                                    Name = (string)x.Element("name"),
+                                    teammember = (string)x.Element("teammembers"),
+                                });
+            Console.WriteLine();
+            
+
+
+        }
+
+        private static void Exercise1_4(string file, string newfile) {
+            
+        }
+    }
+}
