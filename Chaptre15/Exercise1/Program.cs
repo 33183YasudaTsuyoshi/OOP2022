4bcfc3ffbf4e79bb6ad7878cc0561496b00e32dd
33183YasudaTsuyoshi <33183@ojs.ac.jp>
2022-10-06 14:45:00 +0900

    15章　演習問題　2,3

--- Chaptre15/Exercise1/Program.cs
+++ Chaptre15/Exercise1/Program.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 namespace Exercise1 {
     class Program {
         static void Main(string[] args) {
+
+            var years = new List<int>();
+
             Exercise1_2();
             Console.WriteLine();
             Exercise1_3();
@@ -23,11 +26,22 @@ namespace Exercise1 {
 
             Console.ReadLine();
         }
-
+        
         private static void Exercise1_2() {
+            var books = Library.Books.Max(b => b.Price);
+            var book = Library.Books.Where(b => b.Price == books);
+            foreach (var b in book) {
+                Console.WriteLine(b);
+            }
         }
 
         private static void Exercise1_3() {
+            var books = Library.Books.GroupBy(b => b.PublishedYear)
+                                     .OrderBy(o => o.Key);
+            foreach (var book in books) {
+                Console.WriteLine($"{book.Key}年 {book.Count()}冊");
+            }
+            
         }
 
         private static void Exercise1_4() {
