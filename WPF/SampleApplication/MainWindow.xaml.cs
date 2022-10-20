801cc844e0e542957238423a7adc5061896f94de
33183YasudaTsuyoshi <33183@ojs.ac.jp>
2022-10-20 10:18:22 +0900

    くじゲーム完成

--- WPF/SampleApplication/MainWindow.xaml.cs
+++ WPF/SampleApplication/MainWindow.xaml.cs
@@ -20,30 +20,63 @@ namespace SampleApplication {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+
+            seasonComboBox.SelectedIndex = getSeasonIndex();
         }
 
-        private void yellowRadioButton_Checked(object sender, RoutedEventArgs e) {
+        private int getSeasonIndex() {
+            var dt = DateTime.Now;
+            int ret = 0;
+            switch (dt.Month) {
+                case 3:
+                case 4:
+                case 5:
+                    ret = 0;
+                    break;
 
-        }
+                case 6:
+                case 7:
+                case 8:
+                    ret = 1;
+                    break;
 
-        private void blueRadioButton_Checked(object sender, RoutedEventArgs e) {
+                case 9:
+                case 10:
+                case 11:
+                    ret = 2;
+                    break;
 
+                case 12:
+                case 1:
+                case 2:
+                    ret = 3;
+                    break;
+            }
+            return ret;
         }
 
         private void checkBox_Checked(object sender, RoutedEventArgs e) {
-
+            checkBoxTextBlock.Text = "チェック済み";
         }
 
         private void checkBox_Unchecked(object sender, RoutedEventArgs e) {
-
-        }
-
-        private void redRadioButton_Checked(object sender, RoutedEventArgs e) {
-
+            checkBoxTextBlock.Text = "未チェック";
         }
 
         private void seasonComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            seasonTextBlock.Text = (string)((ComboBoxItem)seasonComboBox.SelectedItem).Content;
+        }
 
+        private void yellowRadioButton_Checked(object sender, RoutedEventArgs e) {
+            colorTextBox.Text = "黄";
+        }
+
+        private void blueRadioButton_Checked(object sender, RoutedEventArgs e) {
+            colorTextBox.Text = "赤";
+        }
+
+        private void redRadioButton_Checked(object sender, RoutedEventArgs e) {
+            colorTextBox.Text = "青";
         }
     }
 }
